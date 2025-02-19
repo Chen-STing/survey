491e294b2bcc696d4b5d6ecf004767bcf50c32f0
Jerry <aa29003748@gmail.com>
2025-02-19 17:01:14 +0800

    新增編輯頁面

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login-register/login.component';
 import { TopbarComponent } from './topbar-Home/topbar.component';
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
+import { TabComponent } from './edit-content/tab/tab.component';
+import { AddQuestComponent } from './edit-content/tab/add-quest/add-quest.component';
+import { FeedbackComponent } from './edit-content/tab/feedback/feedback.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent},
-    { path: '', component: TopbarComponent}
-];
+    { path: '', component: TopbarComponent},
+    { path: 'questionnaire', component: QuestionnaireComponent},
+    { path: 'edit', component: TabComponent,
+      children:[
+        { path: 'add', component: AddQuestComponent},
+        { path: 'feedbook', component: FeedbackComponent },
+      ]
+    }
+]
---
+++ src/app/edit-content/edit-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-content',
+  imports: [],
+  templateUrl: './edit-content.component.html',
+  styleUrl: './edit-content.component.scss'
+})
+export class EditContentComponent {
+
+}
