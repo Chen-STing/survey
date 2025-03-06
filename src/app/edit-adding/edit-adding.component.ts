ae499cbb99bc65310c649a626c215ceaeb51785a
Jerry <aa29003748@gmail.com>
2025-03-06 16:03:21 +0800

    更新3/6

--- src/app/edit-adding/edit-adding.component.ts
+++ src/app/edit-adding/edit-adding.component.ts
@@ -10,8 +10,10 @@ import { RouterLink } from '@angular/router';
 })
 export class EditAddingComponent {
 
+  // 選項狀態識別
   questStat: string = "";
 
+  // 選項識別
   selectQuestStat (numberStat :number) {
     switch (numberStat) {
       case 1:
@@ -28,4 +30,30 @@ export class EditAddingComponent {
         break;
     }
   }
+
+  // 新增單選題
+  createRadio () {
+    // 使用 as HTMLElemen 強制轉換類型
+    let radioBox = document.getElementById('radio-box') as HTMLElement;
+
+
+    radioBox.innerHTML += '<i _ngcontent-ng-c2552335061="" class="bx bx-radio-circle"></i><input _ngcontent-ng-c2552335061="" type="text" placeholder="問題選項" class="input is-warning"><i _ngcontent-ng-c2552335061="" class="bx bx-no-entry"></i><br _ngcontent-ng-c2552335061="">'
+  }
+
+  // 新增多選題
+  createCheck () {
+    let checkBox = document.getElementById('check-box') as HTMLElement;
+    console.log(checkBox.innerHTML);
+
+    checkBox.innerHTML += '<i _ngcontent-ng-c2552335061="" class="bx bx-checkbox"></i><input _ngcontent-ng-c2552335061="" type="text" placeholder="問題選項" class="input is-warning"><i _ngcontent-ng-c2552335061="" class="bx bx-no-entry"></i><br _ngcontent-ng-c2552335061="">'
+  }
+
+  remove (e: any) {
+    console.log(e.target);
+
+  }
+
+
 }
+
+
