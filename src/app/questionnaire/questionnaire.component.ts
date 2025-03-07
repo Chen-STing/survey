2dd5a7bb083a391b521cc9bbb0368400f9f0bc5e
Jerry <aa29003748@gmail.com>
2025-03-07 15:53:33 +0800

    更新3/7

--- src/app/questionnaire/questionnaire.component.ts
+++ src/app/questionnaire/questionnaire.component.ts
@@ -56,22 +56,68 @@ export class QuestionnaireComponent {
     this.arrayData.shift()
   }
 
+  // 評量
+  selectStar (e: any) {
+    // 先判斷並刪除所有高亮
+    let star_two = document.getElementById(e.target.id) as HTMLElement;
+    if (star_two.classList.contains('high-star')) {
+      for (let i = 1; i <= 5; i++) {
+        let str_i = String(i);
+        let star = document.getElementById(str_i) as HTMLElement;
+        star.classList.remove('high-star');
+      }
+    }
+    // 再添加高亮
+    for (let i = 1; i <= e.target.id; i++) {
+      let str_i = String(i);
+      let star = document.getElementById(str_i) as HTMLElement;
+      star.classList.add('high-star');
+    }
+  }
+
+
   arrayData: Array<any> = [
     {
       id: 1,
-      title: "貓科動物"
+      title: "貓科動物",
+      questStat: "R",
+      option: [
+        "獅子",
+        "老虎",
+        "花豹",
+        "摺耳貓"
+      ]
     },
     {
       id: 18,
-      title: "第二個"
+      title: "選擇三種你最想成為的動物",
+      questStat: "C",
+      option: [
+        "獅子",
+        "老虎",
+        "花豹",
+        "大象",
+        "狐狸",
+        "狐蒙",
+        "熊貓",
+        "長頸鹿",
+        "黑猩猩",
+        "貓頭鷹",
+        "老鷹",
+        "海豚",
+        "鯨魚",
+        "鱷魚"
+      ]
     },
     {
-      id: 1,
-      title: "p"
+      id: 3,
+      title: "說明你想成為動物的原因",
+      questStat: "T"
     },
     {
       id: 4,
-      title: "f"
+      title: "您推薦木柵動物園嗎？ 推薦指數：",
+      questStat: "S",
     }
   ];
 
