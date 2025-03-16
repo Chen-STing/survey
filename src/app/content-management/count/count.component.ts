81bad9fcaee46c26796b0a6ac54de52242cfc17b
Jerry <aa29003748@gmail.com>
2025-03-16 11:12:56 +0800

    暫存3/16

--- src/app/content-management/count/count.component.ts
+++ src/app/content-management/count/count.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
 import Chart from 'chart.js/auto';
 
@@ -10,7 +10,7 @@ import Chart from 'chart.js/auto';
   templateUrl: './count.component.html',
   styleUrl: './count.component.scss'
 })
-export class CountComponent {
+export class CountComponent implements AfterViewChecked{
 
 
   ngAfterViewChecked(): void {
@@ -21,49 +21,10 @@ export class CountComponent {
 
 
 
-// 長條圖
-// 獲取 canvas 元素
-let ctx_bar = document.getElementById('chart_bar') as HTMLCanvasElement;
-
-// 設定數據
-let data_bar = {
-  // x 軸文字
-  labels: ['1月', '2月', '3月', '4月', '5月', '6月'],
-  datasets: [
-    // 第一組資料
-    {
-      // 上方分類文字
-      label: '月銷售',
-      // 數據
-      data: [30, 20, 40, 32, 45, 24],
-      // 顏色
-      backgroundColor: 'rgba(75, 192, 192, 0.2)',
-      // 邊框顏色
-      borderColor: 'rgba(75, 192, 192, 1)',
-      // 邊框寬度
-      borderWidth: 1,
-    },
-  ],
-};
-
-// 圖表選項
-var options = {
-  scales: {
-    y: {
-      // y 軸從 0 開始
-      beginAtZero: true,
-    },
-  },
-};
-
-// 創建圖表
-new Chart(ctx_bar, {
-  type: 'bar',
-  data: data_bar,
-  options: options,
-});
 
 // 圓餅圖
+let chart_pie: any;
+
 // 獲取 canvas 元素
 let ctx_pie = document.getElementById('chart_pie') as HTMLCanvasElement;
 
@@ -89,12 +50,19 @@ let data_pie = {
   ],
 };
 
+// if(chart_pie) {
+//   chart_pie.destroy();
+// }
+
 // 創建圖表
-let chart = new Chart(ctx_pie, {
+  chart_pie = new Chart(ctx_pie, {
   //pie是圓餅圖,doughnut是環狀圖
   type: 'pie',
   data: data_pie,
 });
+
+
+console.log(chart_pie instanceof Chart);
 
 
   }
